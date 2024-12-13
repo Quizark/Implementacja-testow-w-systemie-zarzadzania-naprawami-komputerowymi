4181c742a074567873633f30d172d28015fb492a
Stanislaw <staszekmendel1029@gmail.com>
2024-12-13 10:16:58 +0100

    Test integracyjny 6 - shouldGetAllPersonsWhenSessionIsValid

---
+++ spring-boot-mongodb/src/main/java/com/example/springbootmongodb/test/PersonControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.springbootmongodb.test;
+
+import com.example.springbootmongodb.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void shouldGetAllPersonsWhenSessionIsValid() throws Exception {
+        String validSessionToken = getLoginToken();
+
+        mockMvc.perform(get("/persons")
+                        .header("Authorization", validSessionToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+
+
+
+    private String getLoginToken() throws Exception {
+        User user = new User();
+        user.setEmail("test@test.test");
+        user.setPassword("!1Password");
+
+        MvcResult result = mockMvc.perform(post("/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        return JsonPath.read(result.getResponse().getContentAsString(), "$.sessionToken");
+    }
+}
